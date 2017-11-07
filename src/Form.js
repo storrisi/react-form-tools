90da528e950958089d8013937fe99c8926cb8065
Simone Torrisi <storrisi@gmail.com>
2017-11-07 15:09:45 +0100

    fix passwordConfirm

--- src/Form.js
+++ src/Form.js
@@ -75,6 +75,9 @@ class Form extends PureComponent {
         case 'passwordChange':
           const passwordChangeProps = {...defaultValues};
           const passwordConfirmProps = {...defaultValues, placeholder:'Password Confirm', name:item.name+'_confirm', key: item.name+'_confirm'};
+          this.props.passwordRenderer ? 
+            itemRenderer = React.cloneElement(this.props.fieldRenderer, {key: item.name+'_container'}, [React.cloneElement(this.props.passwordRenderer, passwordChangeProps, React.cloneElement(this.props.passwordRenderer, passwordConfirmProps))]) : 
+            itemRenderer = React.cloneElement(this.props.fieldRenderer, {key: item.name+'_container'}, [React.cloneElement(<input type="password" />, passwordChangeProps), React.cloneElement(<input type="password" />, passwordConfirmProps)]);
         break;
         case 'submit':
           //RENDER DIRECTLY A BUTTON
